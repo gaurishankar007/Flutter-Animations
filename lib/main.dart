e9e413439738ab2d0f42173311683396269c0dab
gaurishankar007 <gaurisharma358@gmail.com>
2024-04-23 11:23:27 +0545

    fix: navigation

--- lib/main.dart
+++ lib/main.dart
@@ -34,9 +34,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      // initialRoute: "/",
-      // routes: routes,
-      home: const AnimatedToast(),
+      initialRoute: "/",
+      routes: routes,
     );
   }
 }
